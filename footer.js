463d537651684dc54188155d39b8b5d22d8e167c
Fade <168151202+furthings@users.noreply.github.com>
2025-11-04 17:36:23 -0500

    Changed icon image directory path

--- footer.js
+++ footer.js
@@ -6,16 +6,16 @@ const footer_html = `
       <!-- bluesky -->
       <a href="https://bsky.app/profile/furthings.art"><i class="fa-brands fa-bluesky"></i></a>
       <!-- fur affinity -->
-      <a href="https://www.furaffinity.net/user/furthings#"><img src="images/fa-logo.svg"/></a>
+      <a href="https://www.furaffinity.net/user/furthings#"><img src="images/footer/fa-logo.svg"/></a>
       <!-- instagram -->
       <a href="https://www.instagram.com/furthingsart/"><i class="fa-brands fa-instagram"></i></a>
       <!-- tumblr? go here -->
       <!-- toyhouse -->
-      <a href="https://toyhou.se/furthings"><img src="images/toyhouse_icon.svg"/></a>
+      <a href="https://toyhou.se/furthings"><img src="images/footer/toyhouse_icon.svg"/></a>
       <!-- paypal -->
       <a href="https://paypal.me/Avrey"><i class="fa-brands fa-paypal"></i></a>
       <!-- kofi -->
-      <a href="https://ko-fi.com/furthings"><img src="images/kofi-logo.svg"/>
+      <a href="https://ko-fi.com/furthings"><img src="images/footer/kofi-logo.svg"/>
       <!-- etsy -->
       <a href="https://www.etsy.com/shop/furthingsart"><i class="fa-brands fa-etsy"></i></a>
       <!-- discord -->
@@ -23,7 +23,7 @@ const footer_html = `
       <!-- telegram -->
       <a href="about.html"><i class="fa-brands fa-telegram"></i></a>
       <!-- artfight -->
-      <a href="https://artfight.net/~furthings"><img src="images/af-logo.svg"/></a>`;
+      <a href="https://artfight.net/~furthings"><img src="images/footer/af-logo.svg"/></a>`;
 
 /* <!-- patreon -->
       <a href="https://www.patreon.com/furthings/"><i class="fa-brands fa-patreon"></i></a> */
